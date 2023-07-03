1c726d2749e1494d5bf9ff8772a72b3fecf4d231
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-07-02 22:20:51 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('.menu-hamburguer').click(function() {
+        $('.navegacao').slideToggle();
+    })
+
+    $('#button-menu').click(function() {
+        $('.navegacao').slideToggle();
+    })
+})
